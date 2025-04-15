444a622eb89217b8511777bfe2b14a5ff227d04e
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-04-15 22:45:38 +0530

    frontend

--- src/pages/RequestCarouselPage.tsx
+++ src/pages/RequestCarouselPage.tsx
@@ -42,7 +42,7 @@ import { toast } from 'sonner';
 import { cn } from '@/lib/utils';
 import { Badge } from '@/components/ui/badge';
 import { ScrollArea } from "@/components/ui/scroll-area";
-import CarouselPreview from "@/components/CarouselPreview";
+import { CarouselPreview } from "@/components/CarouselPreview";
 import { Label } from "@/components/ui/label";
 import { Avatar } from "@/components/ui/avatar";
 import { useToast } from "../components/ui/use-toast";
