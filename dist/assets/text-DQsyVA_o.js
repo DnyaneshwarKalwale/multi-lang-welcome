269cbdb7bb4853bc5b2dc12c931cc9c25b58c5dd
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-06-19 14:04:11 +0530

    Remove sensitive .env file and add .env.example template - Security fix

---
+++ dist/assets/text-DQsyVA_o.js
@@ -0,0 +1,11 @@
+import{k as t}from"./index-BbVnkZPX.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=t("RotateCw",[["path",{d:"M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8",key:"1p45f6"}],["path",{d:"M21 3v5h-5",key:"1q7to0"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=t("Text",[["path",{d:"M17 6.1H3",key:"wptmhv"}],["path",{d:"M21 12.1H3",key:"1j38uz"}],["path",{d:"M15.1 18H3",key:"1nb16a"}]]);export{e as R,o as T};
